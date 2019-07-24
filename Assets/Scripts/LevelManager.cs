c315cb1178082adfba1009234a242a93e86a2a0f
JuanPabloH <juan@3diq.io>
2019-07-24 17:27:03 -0500

    Add first two systems to test

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Spawn data")]
+    [SerializeField] private GameObject spawnPrefab;
+    [SerializeField] private int amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Entity prefab =  GameObjectConversionUtility.ConvertGameObjectHierarchy(spawnPrefab, World.Active);
+        EntityManager eManager = World.Active.EntityManager;
+
+
+        NativeArray<Entity> objects = new NativeArray<Entity>(amount, Allocator.Temp);
+        eManager.Instantiate(prefab, objects);
+        for (int i = 0; i < amount; i++)
+        {
+            float xVal = UnityEngine.Random.Range(-10.0f,10.0f);
+            float zVal = UnityEngine.Random.Range(-10.0f,10.0f);
+            float yVal = UnityEngine.Random.Range(-10.0f, 10.0f);
+            eManager.SetComponentData(objects[i], new Translation { Value = new float3(xVal, yVal, zVal) });
+            eManager.SetComponentData(objects[i], new Rotation { Value = quaternion.identity });
+            eManager.AddComponentData(objects[i], new RotationData { rotSpeed = 30.0f});
+
+           
+
+        }
+        objects.Dispose();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            World.Active.GetExistingSystem<MoveForwardSystem>().bShouldStop = !World.Active.GetExistingSystem<MoveForwardSystem>().bShouldStop;
+    }
+
+}
---
+++ Assets/Scripts/Systems/MoveForwardSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+[UpdateAfter(typeof(RotationSystem))]
+public class MoveForwardSystem : JobComponentSystem
+{
+    public bool bShouldStop = false;
+
+    [BurstCompile]
+    struct MoveForwardJob : IJobForEach<Translation, Rotation>
+    {
+        public float dt;
+        public float speed;
+        public void Execute(ref Translation translation,[ReadOnly] ref Rotation rotation)
+        {
+            float3 forward = math.rotate(rotation.Value, new float3(0.0f, 0.0f, 1.0f));
+            forward = forward * speed * dt + translation.Value;
+            translation.Value = forward;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        if (bShouldStop) return inputDeps;
+
+        MoveForwardJob job = new MoveForwardJob
+        {
+            dt = Time.deltaTime,
+            speed = 5.0f
+        };
+
+        return job.Schedule(this, inputDeps);
+    }
+}
+
+
+
