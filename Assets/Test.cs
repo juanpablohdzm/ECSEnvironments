dcf1c5779a750b26823690f25a4318f9a2c18089
JuanPabloH <juan@3diq.io>
2019-08-08 11:44:55 -0500

    add curved ui, material for the pedestal.

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 forward = transform.forward;
+        transform.Rotate(transform.right, 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
